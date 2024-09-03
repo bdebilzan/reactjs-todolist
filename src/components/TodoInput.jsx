27ce171dea4f7cf6df0139defe54174026c60d32
bdebilzan <bdebilzan@mac.com>
2024-09-03 15:08:52 -0700

    reactjs-todolist

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+export default function TodoInput(props) {
+    const { handleAddTodos, todoValue, setTodoValue } = props
+
+    return (
+        <header>
+            <input value={todoValue} onChange={(e) => {
+                setTodoValue(e.target.value)
+            }} placeholder="Enter to do..." />
+            <button onClick={() => {
+                handleAddTodos(todoValue)
+                setTodoValue('')
+            }}>Add</button>
+        </header>
+    )
+}
